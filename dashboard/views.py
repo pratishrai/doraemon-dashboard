f050583f26668e255d7b3c1238c49a22b5483e6c
sayam sawai <sawaisayam@gmail.com>
2020-09-12 06:15:26 +0000

    Added Session for User Auth (#1)
    * Added Session for User auth
    
    * Minor change

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,9 +14,31 @@ def auth(request):
 
 
 def user(request):
+
+    if request.session['access_token']:
+
+        user,guild = getData(request.session['access_token'])
+
+        return JsonResponse({'user': user,'guild': guild})
+
     code = request.GET.get('code')
-    user = exchange_code(code)
-    return JsonResponse({'user': user})
+    user,guild,access_token = exchange_code(code)
+
+    request.session['access_token'] = access_token
+    request.session['userID'] = user['id']
+
+    return JsonResponse({'user': user,'guild': guild})
+
+def getData(access_token):
+
+    user_response = requests.get("https://discord.com/api/v6/users/@me", headers={
+        'Authorization': 'Bearer %s' % access_token
+    })
+    guild_response = requests.get("https://discord.com/api/v6/users/@me/guilds", headers={
+        'Authorization': 'Bearer %s' % access_token
+    })
+
+    return user_response.json(), guild_response.json()
 
 
 def exchange_code(code: str):
@@ -34,11 +56,7 @@ def exchange_code(code: str):
     response = requests.post('https://discord.com/api/oauth2/token', data=data, headers=headers)
     credentials = response.json()
     access_token = credentials['access_token']
-    response = requests.get("https://discord.com/api/v6/users/@me", headers={
-        'Authorization': 'Bearer %s' % access_token
-    })
-    guild_response = requests.get("https://discord.com/api/v6/users/@me/guilds", headers={
-        'Authorization': 'Bearer %s' % access_token
-    })
-    user = response.json(), guild_response.json()
-    return user
+
+    user,guild = getData(access_token)
+    
+    return user,guild,access_token
