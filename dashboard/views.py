c3dad9d6939d552b7abfa5c1b80b2ccc725f9bcf
pratishrai <pratishrai724@gmail.com>
2020-09-12 11:59:19 +0530

    generated error

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, redirect
 import requests
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 
 auth_url = "https://discord.com/api/oauth2/authorize?client_id=709321027775365150&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fuser%2F&response_type=code&scope=identify%20email%20guilds"
 
@@ -10,27 +10,28 @@ def index(request):
 
 
 def auth(request):
-    return redirect(auth_url)
+    if request.session['access_token']:
+        return redirect(user)
+    else:
+        return redirect(auth_url)
 
 
 def user(request):
-
     if request.session['access_token']:
+        user, guild = getData(request.session['access_token'])
 
-        user,guild = getData(request.session['access_token'])
-
-        return JsonResponse({'user': user,'guild': guild})
+        return JsonResponse({'user': user, 'guild': guild})
 
     code = request.GET.get('code')
-    user,guild,access_token = exchange_code(code)
+    user, guild, access_token = exchange_code(code)
 
     request.session['access_token'] = access_token
     request.session['userID'] = user['id']
 
-    return JsonResponse({'user': user,'guild': guild})
+    return JsonResponse({'user': user, 'guild': guild})
+
 
 def getData(access_token):
-
     user_response = requests.get("https://discord.com/api/v6/users/@me", headers={
         'Authorization': 'Bearer %s' % access_token
     })
@@ -57,6 +58,6 @@ def exchange_code(code: str):
     credentials = response.json()
     access_token = credentials['access_token']
 
-    user,guild = getData(access_token)
-    
-    return user,guild,access_token
+    user, guild = getData(access_token)
+
+    return user, guild, access_token
