844a9d19bcd6b9708d1f935e811bfcb6fb11e245
pratishrai <pratishrai724@gmail.com>
2020-10-02 00:24:01 +0530

    a lot of stuff

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('auth/', views.auth, name='auth'),
-    path('user', views.user, name='user'),
+    path('user/', views.user, name='user'),
+    path('commands/', views.commands, name='cmds'),
+    path('docs/', views.docs, name='docs'),
+    path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ import requests
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
-auth_url = "https://discord.com/api/oauth2/authorize?client_id=709321027775365150&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fuser&response_type=code&scope=identify%20email%20guilds"
+auth_url = "https://discord.com/api/oauth2/authorize?client_id=709321027775365150&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fuser%2F&response_type=code&scope=identify%20guilds"
 
 
 def index(request):
@@ -26,8 +26,8 @@ def user(request):
     if token:
         user, guild = getData(request.session['access_token'])
 
-        return JsonResponse({'user': user, 'guild': guild})
-        #return render(request, 'user.html', {'user': user, 'guild': guild})
+        #return JsonResponse({'user': user, 'guild': guild})
+        return render(request, 'user.html', {'user': user, 'guild': guild})
 
     code = request.GET.get('code')
     user, guild, access_token = exchange_code(code)
@@ -36,6 +36,12 @@ def user(request):
     request.session['userID'] = user['id']
 
     return redirect(auth)
+
+
+def logout(request):
+    del request.session['access_token']
+    del request.session['userID']
+    return redirect(index)
 
 
 def getData(access_token):
@@ -52,10 +58,10 @@ def getData(access_token):
 def exchange_code(code: str):
     data = {
         "client_id": "709321027775365150",
-        "client_secret": "I_7XyaUS-4QmW5ttSd8lNNt2XXCxd5U6",
+        "client_secret": "rd24BXGOkruso5MXjwaByvfFKjvIExOt",
         "grant_type": "authorization_code",
         "code": code,
-        "redirect_uri": "http://127.0.0.1:8000/user",
+        "redirect_uri": "http://127.0.0.1:8000/user/",
         "scope": "identify email guild"
     }
     headers = {
@@ -68,3 +74,33 @@ def exchange_code(code: str):
     user, guild = getData(access_token)
 
     return user, guild, access_token
+
+
+def commands(request):
+    token = request.session.get('access_token')
+    user = None
+    guild = None
+    if token:
+        user, guild = getData(request.session['access_token'])
+    return render(request, 'commands.html', {'user': user, 'guild': guild})
+
+
+def docs(request):
+    token = request.session.get('access_token')
+    user = None
+    guild = None
+    if token:
+        user, guild = getData(request.session['access_token'])
+    return render(request, 'docs.html', {'user': user, 'guild': guild})
+
+def dashboard(request):
+    token = request.session.get('access_token')
+    user = None
+    guild = None
+    guild_id = request.GET.get('ID')
+    if token:
+        user, guild = getData(request.session['access_token'])
+        for guild_info in guild:
+            if guild_info['id'] == guild_id:
+                fetched_guild_info = guild_info
+    return render(request, 'dashboard.html', {'user': user, 'guild': guild, 'guild_info': fetched_guild_info})
